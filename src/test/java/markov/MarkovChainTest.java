32cbf36c269d3de6a2b82eb79b097503d37aab5a
sankukai7126 <sankukaix@gmail.com>
2020-12-15 10:01:06 +0100

    SENKAYA Mikrail Test read et renforceFalse

--- src/test/java/markov/MarkovChainTest.java
+++ src/test/java/markov/MarkovChainTest.java
@@ -15,7 +15,22 @@ public class MarkovChainTest {
 	
 	@Test
 	public void readTest() {
-		
+		String text = "Hello world";
+		MarkovData data = new MarkovData();
+		data.read(text);
+		String testText = data.getKeyWord(0) + " " + data.getKeyWord(1);
+		int expected = 0;
+		int actual = text.compareTo(testText);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void renforceFalseTest() {
+		String text = "Hello world";
+		MarkovData data = new MarkovData();
+		boolean actual = data.renforceWord("Hello", text);
+		boolean expected = false;
+		assertEquals(expected, actual);
 	}
 
 }
