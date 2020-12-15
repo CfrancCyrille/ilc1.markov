49069e72b3a6af8a3c877b018593d3325514d957
WyderSevrin <sevrin.wyder@gmail.com>
2020-12-15 09:56:09 +0100

    ajout de tests

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -5,10 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MarkovDataTest {
+	MarkovData data = new MarkovData();
 	
-	//@Test
-	public void test() {
-		fail("Not yet implemented");
+        @Test
+	public void test_read_true() {
+		String sentence = "une souris";
+                String newSentence = "";
+                int actual;
+                int expected =0;
+                data.read(sentence);
+                newSentence = data.getKeyWord(0)+" "+data.getKeyWord(1);
+                actual = sentence.compareTo(newSentence);
+                assertEquals(expected, actual);
 	}
-
+        
+        @Test
+	public void test_renforceWord_false() {
+		String sentence = "une souris";
+                Boolean actual;
+                Boolean expected = false;
+                actual = data.renforceWord("une", sentence);
+                assertEquals(expected, actual);
+	}
 }
