4d80c73f382a8d902d8770deb5ac58350fedec8e
lbizot <56951902+lbizot@users.noreply.github.com>
2020-12-15 08:59:57 +0100

    Ajout de deux Tests de la classe MarkovData

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -10,5 +10,26 @@ public class MarkovDataTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testRead() {
+		String depart="jour nuit";
+		MarkovData markov= new MarkovData();
+		markov.read(depart);
+		String assemblage=markov.getKeyWord(0)+" "+markov.getKeyWord(1);
+		int expected=0;
+		int res=depart.compareTo(assemblage);
+		assertEquals(res, expected);
+	}
+	
+	@Test
+	public void testRenforceWord_False() {
+		String depart="jour nuit";
+		MarkovData markov=new MarkovData();
+		boolean res;
+		res=markov.renforceWord("jour",depart );
+		boolean expected=false;
+		assertEquals(expected,res);
+	}
 
 }
