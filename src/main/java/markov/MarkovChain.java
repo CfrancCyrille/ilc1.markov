99233bca94a3ba5bf08327ee6765f80d3d5a1abb
= <peter_bachour@icloud.com>
2020-12-15 08:25:17 +0100

    [ADD] JavaDoc to MarkovChain.java

--- src/main/java/markov/MarkovChain.java
+++ src/main/java/markov/MarkovChain.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+/**
+ *
+ * MarkovChain generates the new chains.
+ *
+ */
 public class MarkovChain {
 	
 	/** Markov chain ngram parameter (contiguous sequence of words) */
