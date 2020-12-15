a381748571a290e00f7b52d325abf9fcf1637b9c
WyderSevrin <sevrin.wyder@gmail.com>
2020-12-15 09:59:52 +0100

    amélioration des tests

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -9,19 +9,19 @@ public class MarkovDataTest {
 	
         @Test
 	public void test_read_true() {
-		String sentence = "une souris";
+		String sentence = "une souris verte";
                 String newSentence = "";
                 int actual;
                 int expected =0;
                 data.read(sentence);
-                newSentence = data.getKeyWord(0)+" "+data.getKeyWord(1);
+                newSentence = data.getKeyWord(0)+" "+data.getKeyWord(1)+" "+data.getKeyWord(1);
                 actual = sentence.compareTo(newSentence);
                 assertEquals(expected, actual);
 	}
         
         @Test
 	public void test_renforceWord_false() {
-		String sentence = "une souris";
+		String sentence = "une souris verte";
                 Boolean actual;
                 Boolean expected = false;
                 actual = data.renforceWord("une", sentence);
